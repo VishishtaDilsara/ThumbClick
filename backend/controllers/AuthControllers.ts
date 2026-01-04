ce8b42e8f39625193c1a9dc5b686c0d0136b742f
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-01-04 12:24:41 +0530

    User Login Created

--- backend/controllers/AuthControllers.ts
+++ backend/controllers/AuthControllers.ts
@@ -35,6 +35,41 @@ export const registerUser = async (req: Request, res: Response) => {
     });
   } catch (err: any) {
     console.log(err);
-    return res.status(500).json({ message: "Something went wrong" });
+    return res
+      .status(500)
+      .json({ message: "Something went wrong when registering" });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    //Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Email or Password" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Email or Password" });
+    }
+
+    //Setting User Data in Session
+    req.session.isLoggedIn = true;
+    req.session.useId = user._id;
+    return res.status(200).json({
+      message: "Login Successfull",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong when logging in" });
   }
 };
