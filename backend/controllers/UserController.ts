584375cc247652ed9cadd1674d5c550eb3178003
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-01-04 14:26:29 +0530

    user controllers created

---
+++ backend/controllers/UserController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import Thumbnail from "../models/Thumbnail.js";
+
+//Controllers to get all user thumbnails
+export const getUserThumbnails = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.session;
+    const thumbnail = await Thumbnail.find({ userId }).sort({ createdAt: -1 });
+    res.status(200).json({ thumbnail });
+  } catch (err: any) {
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+//Controller to get single thumbnail of a user
+export const getThumbnailbyId = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.session;
+    const { id } = req.params;
+
+    const thumbnail = await Thumbnail.findOne({ userId, _id: id });
+    res.json(thumbnail);
+  } catch (err: any) {
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
---
+++ backend/routes/UserRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  getThumbnailbyId,
+  getUserThumbnails,
+} from "../controllers/UserController.js";
+
+const UserRouter = express.Router();
+
+UserRouter.get("/thumbnails", getUserThumbnails);
+UserRouter.get("/thumbnail/:id", getThumbnailbyId);
+
+export default UserRouter;
