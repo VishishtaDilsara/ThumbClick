a51125857db5418d2d13786dbd3ac63fbd43ed7a
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-01-04 09:47:24 +0530

    Thumbnail preview created

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
   colorSchemes,
+  dummyThumbnails,
   type AspectRatio,
   type IThumbnail,
   type ThumbnailStyle,
@@ -26,6 +27,29 @@ const Generate = () => {
   const [style, setStyle] = useState<ThumbnailStyle>("Bold & Graphic");
 
   const [styleDropdownOpen, setStyleDropdownOpen] = useState(false);
+
+  const handleGenerate = async () => {};
+
+  const fetchThumbnail = async () => {
+    if (id) {
+      const thumbnail: any = dummyThumbnails.find(
+        (thumbnail) => thumbnail._id === id
+      );
+      setThumbnail(thumbnail);
+      setAdditionalDetails(thumbnail.user_prompt);
+      setTitle(thumbnail.title);
+      setColorSchemeId(thumbnail.color_scheme);
+      setAspectRatio(thumbnail.aspect_ratio);
+      setStyle(thumbnail.style);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchThumbnail();
+    }
+  });
 
   return (
     <>
@@ -105,7 +129,10 @@ const Generate = () => {
                 </div>
                 {/* BUTTON */}
                 {!id && (
-                  <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
+                  <button
+                    onClick={handleGenerate}
+                    className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors"
+                  >
                     {loading ? "Generating..." : "Generate Thumbnail"}
                   </button>
                 )}
